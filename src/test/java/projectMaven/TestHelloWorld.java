335d36631415e472f4f56a7449249f3b6b10fcc0
Karanveer <Karanveer@DESKTOP-20C73AH>
2020-09-26 20:29:08 +0530

    New Project

---
+++ src/test/java/projectMaven/TestHelloWorld.java
@@ -0,0 +1,31 @@
+package projectMaven;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TestHelloWorld {
+
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+
+	@Test
+	@Order(1)
+	@Disabled
+	public void bhellow1(){
+System.out.println("hello 1");	
+	}
+	
+	@Test
+	@Order(2)
+	public void ahellow2(){
+System.out.println("hello 2");	
+	}
+	
+}
